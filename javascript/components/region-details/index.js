8ac3f60c2c4c8d8835785d5c9bb1724fa49eac41
Artyom Trityak <art.trityak@gmail.com>
2016-05-09 20:23:48 -0700

    Exists

--- javascript/components/region-details/index.js
+++ javascript/components/region-details/index.js
@@ -9,6 +9,7 @@ const RegionDetailsComponent = (props) => {
   //TODO: change key and add styles
   return (
     <div>
+      <h3>{props.title}</h3>
       {props.details.map((line, i) => {
         return (
           <div key={i}>{line}</div>
--- javascript/utils/report-parser/regions.parser.js
+++ javascript/utils/report-parser/regions.parser.js
@@ -7,6 +7,8 @@ const regionRegExp = mRegExp([
   /\s+in\s+.+$/ // ...rest
 ]);
 const ordersRegExp = new RegExp(/^Orders Template \(Long Format\)/);
+const exitsHeaderRegExp = new RegExp(/^Exits:$/);
+const exitsRegExp = new RegExp(/^.+ : (.+) \((\d+),(\d+)(,underworld)?\) in/);
 
 
 export default function regionsParser(rawReport, state) {
@@ -18,41 +20,52 @@ export default function regionsParser(rawReport, state) {
   
   let region = {details: []};
   Object.assign(region, parseRegionHeader(currentSentence.rawRow));
-  Object.assign(state, {__line: currentSentence.nextLine + 1, __modified: true}); // +1 because ------ delimiter
+  Object.assign(state, {__line: currentSentence.nextLine + 1}); // +1 because ------ delimiter
 
 
-  // Go to region details
   // Max 1000 rows in region block 
-  for(let i = 0; i < 1000; i += 1) {
+  for(var i = 0; i < 1000; i += 1) {
     if (rawReport[state.__line].trim() === '') {
       state.__line += 1;
       continue;
     }
+
     region.details.push(currentSentence.rawRow);
-
-
-
-    //Region details processors
-    
-
 
     //Is regions block end
     if (ordersRegExp.test(rawReport[state.__line])) {
       state.regions[generateRegionId(region)] = region;
-      return;
+      break;
     }
 
     // Is it start of new region
     currentSentence = getSentence(rawReport, state.__line);
     if (regionRegExp.test(currentSentence.rawRow) || ordersRegExp.test(currentSentence.rawRow)) {
       state.regions[generateRegionId(region)] = region;
-      return;
+      break;
     }
-    
-    Object.assign(state, {__line: currentSentence.nextLine, __modified: true});
+
+    // Exits: does not a sentence because does not have . or ! in the end, just check this custom case
+    if (exitsHeaderRegExp.test(rawReport[state.__line])) {
+      currentSentence = {
+        rawRow: rawReport[state.__line],
+        nextLine: state.__line + 1
+      };
+    }
+    Object.assign(state, {__line: currentSentence.nextLine});
   }
 
-  throw new Error('Region is too big, line:', state.__line);
+
+  // Parsing exits and add them to the regions if they have not been already added
+  parseAndAddRegionsFromExits(region, state);
+
+
+  Object.assign(state, {__modified: true});
+
+
+  if (i === 1000) {
+    throw new Error('Region is too big, line:', state.__line);
+  }
 }
 
 
@@ -67,9 +80,39 @@ function parseRegionHeader(rawRow, region) {
     x: parseInt(regionHead[2], 10),
     y: parseInt(regionHead[3], 10),
     isUnderworld: regionHead[4] === 'underworld',
-    raw: rawRow
+    title: rawRow
   };
 }
+
+
+function parseAndAddRegionsFromExits(region, state) {
+  // Looking for Exits:
+  for(let i = 0; i < region.details.length; i += 1) {
+    if (!exitsHeaderRegExp.test(region.details[i])) {
+      continue;
+    }
+    
+    // Max 6 border regions
+    for(let j = 1; j < 7; j += 1) {
+      let result = exitsRegExp.exec(region.details[i + j]);
+      if (!result) {
+        return;
+      }
+      let newRegion = {
+        type: result[1],
+        x: result[2],
+        y: result[3],
+        isUnderworld: result[4] === ',underworld',
+        exitRegion: true
+      };
+      let regionId = generateRegionId(newRegion);
+      if (!state.regions[regionId]) {
+        state.regions[regionId] = newRegion;
+      }
+    }
+  }
+}
+
 
 function generateRegionId(region) {
   if (region.isUnderworld) {
